9d9897b5b31453eea9b12d63e2c4337724d9d1ad
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-11-12 10:21:44 -0600

    LoginDiseño
    nada

--- Proyecto/SistemaCursos/SistemaCursos/Login.Designer.cs
+++ Proyecto/SistemaCursos/SistemaCursos/Login.Designer.cs
@@ -3,14 +3,14 @@
     partial class Login
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,98 +20,216 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txt_Usr = new System.Windows.Forms.TextBox();
-            this.txt_Pass = new System.Windows.Forms.TextBox();
-            this.btn_Ingresar = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txt_LogUser = new System.Windows.Forms.TextBox();
+            this.txt_LogPass = new System.Windows.Forms.TextBox();
+            this.btn_IniciarSesion = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_CerrarForm = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_CerrarForm)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 59);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario:";
-            // 
-            // txt_Usr
-            // 
-            this.txt_Usr.Location = new System.Drawing.Point(34, 84);
-            this.txt_Usr.Name = "txt_Usr";
-            this.txt_Usr.Size = new System.Drawing.Size(185, 20);
-            this.txt_Usr.TabIndex = 1;
-            // 
-            // txt_Pass
-            // 
-            this.txt_Pass.Location = new System.Drawing.Point(34, 145);
-            this.txt_Pass.Name = "txt_Pass";
-            this.txt_Pass.PasswordChar = '*';
-            this.txt_Pass.Size = new System.Drawing.Size(185, 20);
-            this.txt_Pass.TabIndex = 2;
-            // 
-            // btn_Ingresar
-            // 
-            this.btn_Ingresar.Location = new System.Drawing.Point(306, 210);
-            this.btn_Ingresar.Name = "btn_Ingresar";
-            this.btn_Ingresar.Size = new System.Drawing.Size(115, 23);
-            this.btn_Ingresar.TabIndex = 3;
-            this.btn_Ingresar.Text = "Iniciar Sesion";
-            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            this.pictureBox1.BackColor = System.Drawing.Color.Green;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 284);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 119);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(232, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Contraseña:";
+            this.label2.Size = new System.Drawing.Size(155, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Iniciar Sesión";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(650, 10);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox3.Location = new System.Drawing.Point(1, 311);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(649, 10);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 19);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(233, 164);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Iniciar Sesión";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contraseña";
             // 
-            // button1
+            // txt_LogUser
             // 
-            this.button1.Location = new System.Drawing.Point(173, 210);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Cancelar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txt_LogUser.BackColor = System.Drawing.Color.White;
+            this.txt_LogUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_LogUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_LogUser.Location = new System.Drawing.Point(237, 124);
+            this.txt_LogUser.Name = "txt_LogUser";
+            this.txt_LogUser.Size = new System.Drawing.Size(311, 28);
+            this.txt_LogUser.TabIndex = 7;
+            // 
+            // txt_LogPass
+            // 
+            this.txt_LogPass.BackColor = System.Drawing.Color.White;
+            this.txt_LogPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_LogPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_LogPass.Location = new System.Drawing.Point(238, 196);
+            this.txt_LogPass.Name = "txt_LogPass";
+            this.txt_LogPass.Size = new System.Drawing.Size(311, 28);
+            this.txt_LogPass.TabIndex = 8;
+            // 
+            // btn_IniciarSesion
+            // 
+            this.btn_IniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_IniciarSesion.FlatAppearance.BorderSize = 0;
+            this.btn_IniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_IniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_IniciarSesion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_IniciarSesion.Location = new System.Drawing.Point(219, 255);
+            this.btn_IniciarSesion.Name = "btn_IniciarSesion";
+            this.btn_IniciarSesion.Size = new System.Drawing.Size(430, 37);
+            this.btn_IniciarSesion.TabIndex = 9;
+            this.btn_IniciarSesion.Text = "Iniciar Sesión";
+            this.btn_IniciarSesion.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox4.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(650, 32);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Green;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(26, 50);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(145, 143);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Iniciar Sesión";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Green;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(11, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(196, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Patos de Tláhuac";
+            // 
+            // btn_CerrarForm
+            // 
+            this.btn_CerrarForm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_CerrarForm.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_CerrarForm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_CerrarForm.Image = ((System.Drawing.Image)(resources.GetObject("btn_CerrarForm.Image")));
+            this.btn_CerrarForm.Location = new System.Drawing.Point(603, 3);
+            this.btn_CerrarForm.Name = "btn_CerrarForm";
+            this.btn_CerrarForm.Size = new System.Drawing.Size(25, 25);
+            this.btn_CerrarForm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_CerrarForm.TabIndex = 14;
+            this.btn_CerrarForm.TabStop = false;
+            this.btn_CerrarForm.Click += new System.EventHandler(this.btn_CerrarForm_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 245);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(649, 321);
+            this.Controls.Add(this.btn_CerrarForm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btn_IniciarSesion);
+            this.Controls.Add(this.txt_LogPass);
+            this.Controls.Add(this.txt_LogUser);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.btn_Ingresar);
-            this.Controls.Add(this.txt_Pass);
-            this.Controls.Add(this.txt_Usr);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Login";
-            this.Text = "Iniciar Sesion ";
-            this.Load += new System.EventHandler(this.Login_Load);
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_CerrarForm)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,13 +237,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txt_Usr;
-        private System.Windows.Forms.TextBox txt_Pass;
-        private System.Windows.Forms.Button btn_Ingresar;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_LogUser;
+        private System.Windows.Forms.TextBox txt_LogPass;
+        private System.Windows.Forms.Button btn_IniciarSesion;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox btn_CerrarForm;
     }
 }
-
--- Proyecto/SistemaCursos/SistemaCursos/Login.cs
+++ Proyecto/SistemaCursos/SistemaCursos/Login.cs
@@ -7,32 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace SistemaCursos
 {
     public partial class Login : Form
     {
-        //Para obtener el string de conexion ir a Ver->Explorador de Servidores->SeleccionarBD->ClickDerecho->Propiedades->ConectionString
-        public static string connectionString = @"Data Source=DESKTOP-GL238MR\SQLEXPRESS;Initial Catalog=sistemaCursos;Integrated Security=True";
-        SqlConnection con = new SqlConnection(connectionString);
-
         public Login()
         {
             InitializeComponent();
         }
 
-        private void Login_Load(object sender, EventArgs e)
+        private void btn_CerrarForm_Click(object sender, EventArgs e)
         {
-
-        }
-        //METODO INICIAR SESIÓN
-        public void iniciarSesion()
-        {
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT * FROM sisemaCursos";
+            Application.Exit();
         }
     }
 }
